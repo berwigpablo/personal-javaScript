655b2e820044319adbe7d4e55fe7f73c81c3416f
pablo <berwigpablo@gmail.com>
2022-05-20 22:33:22 -0300

    RegEx exercise

--- regEx/validation.js
+++ regEx/validation.js
@@ -1,1 +1,33 @@
-// validation script here
+const inputs = document.querySelectorAll('input');
+
+
+const patterns = {
+    telephone:new RegExp(/^[\d]{11}$/),
+    username: new RegExp(/^[a-z\d]{5,12}$/, 'i'),
+    password: new RegExp(/^[\w@-]{8,20}$/),
+    slug: new RegExp(/^[a-z\d-]{8,20}$/),
+    email: new RegExp(/[\w]@[a-z]+\.com/)
+}
+
+// validation function
+
+function validate(field,RegExp){
+    if (RegExp.test(field.value)){
+        field.className = 'valid';
+    } else{
+        field.className = 'invalid';
+    }
+}
+
+inputs.forEach((input) => {
+    input.addEventListener('keyup', (e) =>{
+        //console.log(e.target.attributes.name.value)
+        validate(e.target, patterns[e.target.attributes.name.value])
+    })
+
+
+})
+
+//let reg = /^[a-z]{5,12}$/gi;
+
+//let username = new RegExp(/^[a-z]{5,12}/$, 'i', 'g');
